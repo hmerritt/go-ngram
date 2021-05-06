c3602b62a54661e4d3f28918c0fc6478cdea2345
Harry Merritt <harry@merritt.es>
2021-05-06 03:37:52 +0100

    String to ngram conversion function

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
+import "encoding/base64"
+
 // Default ngram length
-const ngram = 3
+const ngramDefault = 3
 
 type NgramIndex struct {
 	// Index of trigrams
@@ -26,6 +28,29 @@ func NewNgramIndex() *NgramIndex {
 	t := new(NgramIndex)
 	t.NgramMap = make(map[string]map[int]struct{})
 	t.IndexesMap = make(map[int]struct{})
-	t.Ngram = ngram
+	t.Ngram = ngramDefault
 	return t
+}
+
+/*
+ * Ngram slice
+ * splits a string into groups of 3,
+ * used for indexing and fast searching
+ */
+func StringToNgram(s string, ngram int) []string {
+	trigrams := make([]string, 0)
+
+	if len(s) == 0 {
+		return trigrams
+	}
+
+	for i := 0; i < len(s)-(ngram-1); i++ {
+		// Encode string to base 64,
+		// this cleans up map keys with
+		// special chars
+		s64 := base64.StdEncoding.EncodeToString([]byte(s[i : i+ngram]))
+		trigrams = append(trigrams, s64)
+	}
+
+	return trigrams
 }
